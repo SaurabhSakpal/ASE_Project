fffcaac58c30d3493e0dd9a7454dee85f18b2d47
SaurabhSakpal <sfstoe@gmail.com>
2016-11-18 10:01:16 -0500

    Adding MobilePhone.xml

--- variance.py
+++ variance.py
@@ -8,18 +8,18 @@ def main():
     assert len(sys.argv) == 2, "SPLOT Parser takes path to model.xml file as argument"
     modelFile = sys.argv[1]
     model = SPLOTParser().parse(modelFile)
-    model.printTree(model.root, 0)
-    print "\n\n ALL CROSS TREE CONSTRAINTS"
-    model.printCrossTreeConstraints()
-    print "\n Generating Tree level Constraints using grammar ... "
+    #model.printTree(model.root, 0)
+    #print "\n\n ALL CROSS TREE CONSTRAINTS"
+    #model.printCrossTreeConstraints()
+    #print "\n Generating Tree level Constraints using grammar ... "
     model.generateTreeStructureConstraints(model.root)
-    print "\n\n ALL TREE STRUCTURE CONSTRAINTS"
-    model.printTreeConstraints()
+    #print "\n\n ALL TREE STRUCTURE CONSTRAINTS"
+    #model.printTreeConstraints()
     #print "\n\n"
     #model.printStatistics()
 
     simulator = Simulator(model)
-    n = 1000
+    n = 1
     cost = []
     violations = []
     num_features = []
@@ -29,19 +29,19 @@ def main():
     for i,point in enumerate(points):
         # print '*******POINT '+str(i)+' ********'
         point.evaluateObjectives()
-        #print point.objectives
-        cost.append(point.objectives.cost)
-        violations.append(point.objectives.constraintsFailed)
-        num_features.append(point.objectives.featureRichness)
+        print point.objectives
+        #cost.append(point.objectives.cost)
+        #violations.append(point.objectives.constraintsFailed)
+        #num_features.append(point.objectives.featureRichness)
 
     runOptimiser(points, simulator, model)
 
-    print 'Mean cost', np.mean(cost)
-    print 'Var cost', np.var(cost)
-    print 'Mean violations', np.mean(violations)
-    print 'Var violations', np.var(violations)
-    print 'Mean num of features', np.mean(num_features)
-    print 'Var num of features', np.var(num_features)
+    #print 'Mean cost', np.mean(cost)
+    #print 'Var cost', np.var(cost)
+    #print 'Mean violations', np.mean(violations)
+    #print 'Var violations', np.var(violations)
+    #print 'Mean num of features', np.mean(num_features)
+    #print 'Var num of features', np.var(num_features)
     # plt.scatter(cost,num_features)
     # plt.show()
     # plt.scatter(violations,num_features)
