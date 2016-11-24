f1653b49b9883ca078befd2dd3419ccea6719d91
SaurabhSakpal <sfstoe@gmail.com>
2016-11-24 09:36:52 -0500

    Adding one code for GA

--- Optimisers.py
+++ Optimisers.py
@@ -147,12 +147,80 @@ def spea2(simulator, model):
     return pop
 
 
-def pso():
+def ga(simulator, model):
     """ PSO implementation goes here """
+    simulator.setPopulationSize(100)
+    simulator.generateInitialPopulation()
+    creator.create("FitnessMulti", base.Fitness, weights=(-1.0, -1.0, 1.0), crowding_dist=None)
+    creator.create("Individual", list, fitness=creator.FitnessMulti)
+    toolbox = base.Toolbox()
+    toolbox.register("splot_point", getSATSolverGeneratedPoint, simulator)
+    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.splot_point)
+    toolbox.register("evaluate", evaluateObjectives, model)
+    toolbox.register("select", tools.selSPEA2)
+    toolbox.register("mate", matePoints, model)
+    toolbox.register("mutate", mutatePoints, model)
+    # binary tournament selection
+    toolbox.register("selectTournament", tools.selTournament, tournsize=2)
+    # ind1 = toolbox.individual()
+    # print ind1
+    # print ind1.fitness.valid
+    MU = 20
+    CXPB, MUTPB, NGEN = 0.5, 0.2, 100
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    pop = toolbox.population(n=MU)
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    for g in range(NGEN):
+        print("-- Generation %i --" % g)
+
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+            # cross two individuals with probability CXPB
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+
+                # fitness values of the children
+                # must be recalculated later
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+
+            # mutate an individual with probability MUTPB
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        print("  Evaluated %i individuals" % len(invalid_ind))
+
+        # The population is entirely replaced by the offspring
+        pop[:] = offspring
+
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+
+    printPopulation(pop)
+
 
 
 def runOptimiser(simulator, model):
-   pop = spea2(simulator, model)
+   pop = ga(simulator, model)
    # cost = [t.fitness.values[0] for t in pop]
    # fr = [t.fitness.values[2] for t in pop]
    # plt.scatter(cost,fr)
