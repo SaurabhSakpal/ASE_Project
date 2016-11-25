9850b65b29052d8a9af49b77379880d8094023fd
ssharm21 <ssharm21@ncsu.edu>
2016-11-24 20:24:11 -0500

    added code to calculate hyper volume

--- variance.py
+++ variance.py
@@ -3,52 +3,44 @@ import numpy as np
 from parser import SPLOTParser
 from Simulator import Simulator
 from Optimisers import *
+import GAMethods as ga
 
 def main():
     assert len(sys.argv) == 2, "SPLOT Parser takes path to model.xml file as argument"
     modelFile = sys.argv[1]
     model = SPLOTParser().parse(modelFile)
-    #model.printTree(model.root, 0)
-    #print "\n\n ALL CROSS TREE CONSTRAINTS"
-    #model.printCrossTreeConstraints()
-    #print "\n Generating Tree level Constraints using grammar ... "
     model.generateTreeStructureConstraints(model.root)
-    #print "\n\n ALL TREE STRUCTURE CONSTRAINTS"
-    #model.printTreeConstraints()
-    #print "\n\n"
-    #model.printStatistics()
 
     simulator = Simulator(model)
     n = 1000
-    cost = []
+    costs = []
     violations = []
     num_features = []
+    
+    # print ga.getIndividualPoint(simulator)
+    # exit()
+    simulator.generateInitialPopulation()
+    points = simulator.startPopulation
+    non_violated_points = 0
+    for point in points:
+        #point.evaluateObjectives()
+        cost, violation , num_feat = ga.evaluateObjectives(model,point)
+        costs.append(cost)
+        violations.append(violation)
+        if violation ==0:
+            non_violated_points += 1
+        num_features.append(num_feat)
 
-    # print "\n\nGENERATING " + str(n) + " POINTS"
-    # points = simulator.generateNPoints(n)
-    # print model.nodeOrder
-    # for i,point in enumerate(points):
-    #     print '*******POINT '+str(i)+' ********'
-    #     point.evaluateObjectives()
-    #     print point.objectives
-    #     cost.append(point.objectives.cost)
-    #     violations.append(point.objectives.constraintsFailed)
-    #     num_features.append(point.objectives.featureRichness)
-
-    runOptimiser(simulator, model)
-
-    # print 'Mean cost', np.mean(cost)
-    # print 'Var cost', np.var(cost)
-    # print 'Mean violations', np.mean(violations)
-    # print 'Var violations', np.var(violations)
-    # print 'Mean num of features', np.mean(num_features)
-    # print 'Var num of features', np.var(num_features)
-    # plt.scatter(cost,num_features)
-    # plt.show()
+    print 'Mean cost', np.mean(costs)
+    print 'Var cost', np.var(costs)
+    print 'Mean violations', np.mean(violations)
+    print 'Var violations', np.var(violations)
+    print 'Mean num of features', np.mean(num_features)
+    print 'Var num of features', np.var(num_features)
+    plt.scatter(costs,num_features)
+    plt.show()
     # plt.scatter(violations,num_features)
     # plt.show()
-    #print "\n\n\n"
-    #print sorted(model.featureFailureCount.items())
 
 if __name__ == "__main__":
     main()
