510752b94556e8185b611139819a853395177b5b
gurunathsagar <gurunath.sagar@gmail.com>
2016-11-29 12:21:01 -0500

    Added matplotlib

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt-get install python-pip
 sudo apt-get install python-dev
 sudo pip install pycosat
 sudo apt-get install python-scipy
-sudo pip install matplotlib
+sudo apt-get install python-matplotlib
